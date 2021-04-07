9a4f63dbfc23519ecb7b61ff8497329b08e99df0
Filipe <pi.pinho2@hotmail.com>
2021-04-07 01:27:03 +0100

    Implementei menu

--- 1st_proj/game.cpp
+++ 1st_proj/game.cpp
@@ -15,16 +15,32 @@
 using namespace std;
 
 
-
-// void setcolor(unsigned int color){
-//     SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
-// }
-
 void draw_menu(){
-    /*setcolor(9);*/ cout << "                 ROBOTS GAME                 ";/* setcolor(7); */cout << endl;
+    cout << "-----------------ROBOTS GAME-----------------" << endl;
     cout << "                 1) Rules                 " << endl;
     cout << "                 2) Play                 " << endl;
     cout << "                 0) Exit                 " << endl;
+}
+
+bool file_exists(string file_name){
+    ifstream file;
+    file.open(file_name);
+
+    if(file){
+        return true;
+    }
+    return false;
+}
+
+void create_file(string file_name){
+
+    ofstream file;
+    file.open(file_name, ios::out);
+    if(!file){
+        cout << "Error to open file!" << endl;
+        return ;
+    }
+    file.close();
 }
 
 int play_game(){
@@ -36,63 +52,98 @@ int play_game(){
     cout << "\033[2J\033[1;1H";
 
     while(1){
+
         file_name = "MAZE_";
+
         cout << "Enter the maze size: ";
         cin >> maze_value;
         
-        if(maze_value > 0 && maze_value <= 9){
+        if(cin.fail()){
+
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << "You have entered wrong input" << endl;
+
+            if(cin.eof()){
+                exit(0);
+            }
+
+        } else if(maze_value > 0 && maze_value <= 9){
+
             aux = to_string(maze_value);
             file_name = file_name + "0" + aux + ".TXT";
-        }
-        else if(maze_value >= 10 && maze_value <= 99){
+
+            if(!file_exists(file_name)){
+                create_file(file_name);
+                file_name.clear();
+            } else {
+
+                cout << "Maze already exists" << endl;
+                break;
+            }
+
+        } else if(maze_value >= 10 && maze_value <= 99){
+
             aux = to_string(maze_value);
             file_name = file_name + aux + ".TXT";
-        }
-        else if(maze_value == 0){
+
+            if(!file_exists(file_name)){
+                create_file(file_name);
+                file_name.clear();
+            } else {
+
+                cout << "Maze already exists" << endl;
+                break;
+            }
+
+        } else if(maze_value == 0){
             //clear terminal
             cout << "\033[2J\033[1;1H";
             return 0;
-        }
-        else{
-            cout << "Enter a valid value (between 1 and 99)" << endl;
-        }
 
-        ofstream file;
-        file.open(file_name, ios::out);
-        if(!file){
-            cout << "Error to open file!" << endl;
-            return -1;
         }
-        file.close();
-        file_name.clear();
     }
     return 1;
 }
 
 int main(){
-    char cmenu;
+    int cmenu;
 
     draw_menu();
 
+
     while(1){
         cin >> cmenu;
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
 
-        if(cmenu == '1'){
+        if(cin.fail()){
+
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << "You have entered wrong input" << endl;
+
+            if(cin.eof()){
+                exit(0);
+            }
+
+        } else if( cmenu == 1){
             cout << "RULES" << endl;
             break;
-        }else if(cmenu == '2'){
-            cout << "PLAY" << endl;
-            if(play_game() == 0){
+
+        } else if(cmenu == 2){
+
+            int aux = play_game();
+
+            if(aux == 0){
                 draw_menu();
+                cout << "TEST";
+            } else if(aux == 1){
+                cout << " MAZE FILE FOUND!" << endl;
+                break;
             }
-            break;
-        }else if(cmenu == '0'){
-            cout << "EXIT" << endl;
-            return 0;
-        }
-        else{
-            cout << "Enter a valid character please!" << endl;
+
+        } else if( cmenu == 0){
+            cout << "Exit" << endl;
+            exit(0);
         }
     }
         
