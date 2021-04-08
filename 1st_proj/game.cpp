4804ef7ebd25998cfcc752f329b9b6195547da6e
Filipe <pi.pinho2@hotmail.com>
2021-04-08 04:23:29 +0100

    Mudei cenas no menu

--- 1st_proj/game.cpp
+++ 1st_proj/game.cpp
@@ -6,7 +6,6 @@
 #include<ios> 
 #include <algorithm>
 #include <limits>
-#include <curses.h>
 #include <stdio.h>
 #include <fstream>
 #include <string>
@@ -16,7 +15,7 @@ using namespace std;
 
 
 
-int draw_menu(){
+int draw_menu(bool &rules, bool &play, bool &exits){
     int cmenu;
 
 
@@ -40,31 +39,35 @@ int draw_menu(){
             }
 
         } else if( cmenu == 1){
-            return 1;
+            rules = true;
+            break;
 
         } else if(cmenu == 2){
-            return 2;
+            play = true;
+            break;
 
         } else if( cmenu == 0){
-            return 0;
+            exits = true;
+            break;
         }
         else{
             cout << "Enter a valid number! (0,1,2)" << endl;
         }
     }
-    return -1;
+    return 0;
 }
 
-bool file_exists(string file_name){
-    ifstream file;
-    file.open(file_name);
+//verifies if a file exists
+bool file_exists(const string &file_name){
+    ifstream file (file_name);
 
-    if(file){
-        return true;
-    }
-    return false;
+    if(!file){
+        return false;
+    } else return true;
+
 }
 
+//create file with name file_name
 void create_file(string file_name){
 
     ofstream file;
@@ -76,7 +79,27 @@ void create_file(string file_name){
     file.close();
 }
 
-int play_game(){
+
+
+void read_file(string &file_name){
+    int lines, columns;
+    fstream file;
+    file.open(file_name, ios::in);
+
+    if(!file){
+        cout << "Can't open file" << endl;
+    } else{
+        string line;
+        getline(file, line);
+        for (size_t i = 0; i < line.size(); i++){
+            cout << line[i] << " - " << (int)line[i] << endl;
+        }        
+    }
+    file.close();
+}
+
+
+int play_game(bool &menu){
     int maze_value;
     string file_name;
     string aux;
@@ -106,11 +129,14 @@ int play_game(){
             aux = to_string(maze_value);
             file_name = file_name + "0" + aux + ".TXT";
 
-            if(!file_exists(file_name)){
-                create_file(file_name);
+            if(file_exists(file_name)){
+                cout << "File exists" << endl;
+                read_file(file_name);
                 file_name.clear();
+                menu = false;
+                break;
             } else {
-                return 1;
+                cout << "File doesn't exist" << endl;
             }
 
         } else if(maze_value >= 10 && maze_value <= 99){
@@ -118,17 +144,20 @@ int play_game(){
             aux = to_string(maze_value);
             file_name = file_name + aux + ".TXT";
 
-            if(!file_exists(file_name)){
-                create_file(file_name);
+            if(file_exists(file_name)){
+                cout << "File exists" << endl;
+                read_file(file_name);
                 file_name.clear();
+                menu = false;
+                break;
             } else {
-
-                return 1;
+                cout << "File doesn't exist" << endl;
             }
 
         } else if(maze_value == 0){
             //clear terminal
             cout << "\033[2J\033[1;1H";
+            menu = true;
             return 0;
 
         } else{
@@ -140,34 +169,32 @@ int play_game(){
 
 int main(){
 
-    int menu = draw_menu();
-    int aux;
+    bool menu = true, play = false, rules = false, exits = false;
 
-    switch (menu){
-    case 1:
 
-        cout << "RULES" << endl;
-        break;
+    while(menu){
 
-    case 2:
+        draw_menu(rules, play, exits);
 
-        aux = play_game();
+        if(play){
+            cout << "Playing game" << endl;
+            play_game(menu);
+            play = false;
+        }
+        
+        if(rules){
+            cout << "RULES" << endl;
+            rules = false;
+            menu = false;
+        }
 
-        if(aux == 0){
-            draw_menu();
-        } else if(aux == 1){
-            cout << " MAZE FILE FOUND!" << endl;
-            break;
-            }
-
-        break;
-
-    case 0:
-
-        cout << "Exit" << endl;
-        exit(0);
+        if(exits){
+            cout << "exit" << endl;
+            exits = false;
+            menu = false;
+            exit(0);
+        }
 
     }
-
     return 0;
 }
