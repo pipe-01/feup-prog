1b525cf290e60ff326110d124730462e52aef0f7
juan <juan.bellon2001@hotmail.com>
2021-04-15 00:04:17 +0100

    Ve as funções do read_file

--- 1st_proj/main.cpp
+++ 1st_proj/main.cpp
@@ -39,16 +39,6 @@ struct Robot
 
 vector<Robot> robots; //Accessed several times
 
-void printBoard(vector<vector<char>> board){
-    cout << "\033[2J\033[1;1H"; 
-    for (size_t i = 0; i < board.size(); i++){
-        for (size_t j = 0; j < board[0].size(); j++){
-           cout << board[i][j];
-        }
-        
-    }
-    cout << endl;
-}
 
 void wait() {
     cout << "\n\tPress ENTER key to continue....";
@@ -121,6 +111,82 @@ int draw_menu(bool &rules, bool &play, bool &exits)
     return 0;
 }
 
+void printRules(){
+
+    //clear terminal
+    cout << "\033[2J\033[1;1H";
+
+    char any;
+    cout << "                                ___       __      \n"
+            "                               / _ \\__ __/ /__ ___\n"
+            "                              / , _/ // / / -_|_-<\n"
+            "                             /_/|_|\\_,_/_/\\__/___/\n"
+            "                                                  " << endl;
+    cout << "H = human, R = Robot, * = Fence/Post\n"
+            "1) Main Goal of the Game: Survive the Robots & Electrical Posts/Fences\n"
+            "2) Win is achieved if no robots are alive\n"
+            "3) Dead robots remain in their last position (thus can not move through them)\n"
+            "4) For every move, robots move one tile closer to your character (H)\n"
+            "5) Death can be by collision with robots or fences/posts\n"
+            "6) Robots also die from collisions between each other (only one robot is placed in the place of death)\n"
+            "7) Times are recorded and stored in a text file\n"
+            "Controls are as follows: \n"
+            "\tA/a = Left\n"
+            "\tW/w = Up\n"
+            "\tD/d = Right\n"
+            "\tX/x = Down\n"
+            "\tS/s = No movement\n"
+            "\tQ/q = Up-left\n"
+            "\tE/e = Up-right\n"
+            "\tZ/z = Down-left\n"
+            "\tC/c = Down-right\n"
+            "\t0 = Exit Game\n";
+    wait();
+
+    //clear terminal
+    cout << "\033[2J\033[1;1H";
+}
+
+void printDeadRobotCollision(){
+    cout << "  ___     _ _ _    _                           _         _   \n"
+            " / __|___| | (_)__(_)___ _ _    __ _ __ _ __ _(_)_ _  __| |_ \n"
+            "| (__/ _ \\ | | (_-< / _ \\ ' \\  / _` / _` / _` | | ' \\(_-<  _|\n"
+            " \\___\\___/_|_|_/__/_\\___/_||_| \\__,_\\__, \\__,_|_|_||_/__/\\__|\n"
+            "                                    |___/                    \n"
+            "    _             _           _         _       _            \n"
+            " __| |___ __ _ __| |  _ _ ___| |__  ___| |_    | |_ _ _ _  _ \n"
+            "/ _` / -_) _` / _` | | '_/ _ \\ '_ \\/ _ \\  _|_  |  _| '_| || |\n"
+            "\\__,_\\___\\__,_\\__,_| |_| \\___/_.__/\\___/\\__( )  \\__|_|  \\_, |\n"
+            "                                           |/           |__/ \n"
+            "                _      \n"
+            " __ _ __ _ __ _(_)_ _  \n"
+            "/ _` / _` / _` | | ' \\ \n"
+            "\\__,_\\__, \\__,_|_|_||_|\n"
+            "     |___/            " << endl;
+}
+
+void printExit(){
+
+    //clear terminal
+    cout << "\033[2J\033[1;1H";
+
+    cout << " ____                                  _       _            _ \n"
+            "/ ___|  ___  ___   _   _  ___  _   _  | | __ _| |_ ___ _ __| |\n"
+            "\\___ \\ / _ \\/ _ \\ | | | |/ _ \\| | | | | |/ _` | __/ _ \\ '__| |\n"
+            " ___) |  __/  __/ | |_| | (_) | |_| | | | (_| | ||  __/ |  |_|\n"
+            "|____/ \\___|\\___|  \\__, |\\___/ \\__,_| |_|\\__,_|\\__\\___|_|  (_)\n"
+            "                   |___/                                      " << endl;
+}
+
+void printInvalidChar(){
+    cout << " ___     _                             _ _    _   _                _   _ \n"
+            "| __|_ _| |_ ___ _ _   __ _  __ ____ _| (_)__| | (_)_ _  _ __ _  _| |_| |\n"
+            "| _|| ' \\  _/ -_) '_| / _` | \\ V / _` | | / _` | | | ' \\| '_ \\ || |  _|_|\n"
+            "|___|_||_\\__\\___|_|   \\__,_|  \\_/\\__,_|_|_\\__,_| |_|_||_| .__/\\_,_|\\__(_)\n"
+            "                                                        |_|              " << endl;
+}
+
+
 //check if a file exists
 bool file_exists(const string &file_name)
 {
@@ -167,28 +233,33 @@ void read_file(string &file_name, vector<vector<char>> &tiles)
         file >> rows >> useless >> columns;
         //clear buffer
         file.ignore(256, '\n');
-        vector<vector<char>> tiles (rows, vector<char> (columns));
+
+        tiles.resize(rows);
+        for (int i = 0; i < tiles.size(); i++){
+            tiles[i].resize(columns);
+        }
+
 
         for (int i = 0; i < rows; i++){
             getline(file, line);
-                for (int j = 0; j < columns; j++){
-                    tiles[i][j] = line[j];
-                    if(line[j] == LIVEHUMAN){
-                        player.x = j;
-                        player.y = i;
-                        cout << "Player position :" << player.x << ' ' << player.y << endl;
-                    } else if(line[j] == LIVEROBOT){
-                        Robot r1 = Robot(j, i);
-                        robots.push_back(r1);
-                        cout << "Robot position :" << j << ' ' << i << endl;
-                    }
+            for (int j = 0; j < columns; j++){
+                tiles[i][j] = line[j];
+                if(line[j] == LIVEHUMAN){
+                    player.x = j;
+                    player.y = i;
+                    cout << "Player position :" << player.x << ' ' << player.y << endl;
+                } else if(line[j] == LIVEROBOT){
+                    Robot r1 = Robot(j, i);
+                    robots.push_back(r1);
+                    cout << "Robot position :" << j << ' ' << i << endl;
                 }
+            }
         }
     }
     file.close();
 }
 
-int read_game(bool &menu, vector<vector<char>> &tiles, struct Player &player)
+int read_game(bool &menu, vector<vector<char>> &tiles, Player &player)
 {
     int maze_value;
     string file_name;
@@ -284,13 +355,44 @@ void create_file(string file_name)
     file.close();
 }
 
-void movePlayer(vector<vector<char>> &tiles, struct Player &player){
-    char move;
+void placePlayer(vector<vector<char>> &tiles, Player player, int prevX, int prevY){
+    tiles[player.x][player.y] = LIVEHUMAN;
+    tiles[prevX][prevY] = ' ';
+}
+
+//0 -> Collides and Kills player
+//1 -> Collides against dead robot (survives)
+//2 -> Valid
+char checkCollision(vector<vector<char>> &tiles, Player &player){
+    if(tiles[player.x][player.y] != ' '){
+        if(tiles[player.x][player.y] == '*'){
+            player.isAlive = false;
+            return '0';
+        }
+        else{
+            if(tiles[player.x][player.y] == DEADROBOT){
+                printDeadRobotCollision();
+                return '1';
+            }
+            else{
+                player.isAlive = false;
+                return '0';
+            }
+        }
+    }
+    else{
+        return '2';
+    }
+}
+
+void movePlayer(vector<vector<char>> &tiles, Player &player){
+    int prevX = player.x, prevY = player.y;
+    char move, coll;
 
     while(1){
-
-        cout << "Enter the movement that you want :" << endl;
+        cout << "Enter movement player: ";
         cin >> move;
+
 
         if (cin.fail()){
 
@@ -303,13 +405,22 @@ void movePlayer(vector<vector<char>> &tiles, struct Player &player){
             }
         }
         else if(move == 'A' || move == 'a'){
+            // if(checkCollision(tiles, player.x, player.y, player.x--, player.y)){
+            //     player.isAlive = false;
+            // }
             player.x--;
+
         }
         else if(move == 'D' || move == 'd'){
+            // if(checkCollision(tiles, player.x, player.y, player.x++, player.y)){
+            //     player.isAlive = false;
+            // }
             player.x++;
+
         }
         else if(move == 'W' || move == 'w'){
             player.y--;
+
         }
         else if(move == 'X' || move == 'x'){
             player.y++;
@@ -329,68 +440,38 @@ void movePlayer(vector<vector<char>> &tiles, struct Player &player){
         else if(move == 'C' || move == 'c'){
             player.x++;
             player.y++;
+
         }
-        else if(move == '0' ){
-            break;
+        else if(move == 'S' || move == 's'){
+
         }
+        else if(move == '0'){
+            player.isAlive = false;
+        }
+        else{
+            printInvalidChar();
+            continue;
+        }
+        //Basicamente esta parte aqui primeiro verifica se ha algum tipo de colisão e se sim, qual. Depois
+        //Posiciona o jogador na posição nova caso não exista nenhum tipo de colisão
+        coll = checkCollision(tiles,player);
+        if(coll == '2'){
+            placePlayer(tiles,player,prevX, prevY);
+        }
+        else if(coll == '1'){
+            player.x = prevX;
+            player.y = prevY;
+        }
+        break;
     }
 }
 
 
-void playGame(vector<vector<char>> &tiles, struct Player &player){  
+void playGame(vector<vector<char>> &tiles, struct Player &player){
     while(player.isAlive){
-        movePlayer(tiles, player);
         drawMaze(tiles);
+        movePlayer(tiles, player);
     }
-}
-
-void printRules(){
-
-    //clear terminal
-    cout << "\033[2J\033[1;1H";
-
-    char any;
-    cout << "                                ___       __      \n"
-            "                               / _ \\__ __/ /__ ___\n"
-            "                              / , _/ // / / -_|_-<\n"
-            "                             /_/|_|\\_,_/_/\\__/___/\n"
-            "                                                  " << endl;
-    cout << "H = human, R = Robot, * = Fence/Post\n"
-            "1) Main Goal of the Game: Survive the Robots & Electrical Posts/Fences\n"
-            "2) Win is achieved if no robots are alive\n"
-            "3) Dead robots remain in their last position (thus can not move through them)\n"
-            "4) For every move, robots move one tile closer to your character (H)\n"
-            "5) Death can be by collision with robots or fences/posts\n"
-            "6) Robots also die from collisions between each other (only one robot is placed in the place of death)\n"
-            "7) Times are recorded and stored in a text file\n"
-            "Controls are as follows: \n"
-            "\tA/a = Left\n"
-            "\tW/w = Up\n"
-            "\tD/d = Right\n"
-            "\tX/x = Down\n"
-            "\tS/s = No movement\n"
-            "\tQ/q = Up-left\n"
-            "\tE/e = Up-right\n"
-            "\tZ/z = Down-left\n"
-            "\tC/c = Down-right\n"
-            "\t0 = Exit Game\n";
-    wait();
-
-    //clear terminal
-    cout << "\033[2J\033[1;1H";
-}
-
-void printExit(){
-
-    //clear terminal
-    cout << "\033[2J\033[1;1H";
-
-    cout << " ____                                  _       _            _ \n"
-            "/ ___|  ___  ___   _   _  ___  _   _  | | __ _| |_ ___ _ __| |\n"
-            "\\___ \\ / _ \\/ _ \\ | | | |/ _ \\| | | | | |/ _` | __/ _ \\ '__| |\n"
-            " ___) |  __/  __/ | |_| | (_) | |_| | | | (_| | ||  __/ |  |_|\n"
-            "|____/ \\___|\\___|  \\__, |\\___/ \\__,_| |_|\\__,_|\\__\\___|_|  (_)\n"
-            "                   |___/                                      " << endl;
 }
 
 int main()
@@ -409,13 +490,13 @@ int main()
         {
             cout << "Playing game" << endl;
             read_game(menu, tiles, player);
+            //Depois desta função o valor de player.x e player.y não é alterado
             play = false;
-            printBoard(tiles);
             playGame(tiles, player);
         }
 
         if (rules)
-        { //Por fazer
+        {
             printRules();
             rules = false;
         }
